96eacbdcabf5a21910b9fa7e2b337d01d498d1d2
Yewon Kwon <sangdo90@naver.com>
2025-05-21 16:22:46 +0900

    시작 메뉴 버튼-> 게임 연결 시험 중
    방향키 계속 누르고 있으면 생기는 딜레이 수정.
    keydown 이벤트 리스너 변경, keyup 이벤트 리스너 추가.
    movePlayer 변경.
    
    블럭 여러 개 깨질 때 생기는 오류 수정 중

--- breakout.js
+++ breakout.js
@@ -2,20 +2,20 @@
 let board;
 let boardWidth = 500;
 let boardHeight = 500;
-let context; 
+let context;
 
 //players
-let playerWidth = 80; //500 for testing, 80 normal
+let playerWidth = 100; //500 for testing, 80 normal
 let playerHeight = 10;
-let playerVelocityX = 10; //move 10 pixels each time
+let playerVelocityX = 5; //프레임당 10px 이동
 
 let player = {
-    x : boardWidth/2 - playerWidth/2,
-    y : boardHeight - playerHeight - 5,
-    width: playerWidth,
-    height: playerHeight,
-    velocityX : playerVelocityX
-}
+  x: boardWidth / 2 - playerWidth / 2,
+  y: boardHeight - playerHeight - 5,
+  width: playerWidth,
+  height: playerHeight,
+  velocityX: playerVelocityX,
+};
 
 //ball
 let ballWidth = 10;
@@ -24,208 +24,250 @@ let ballVelocityX = 3; //15 for testing, 3 normal
 let ballVelocityY = 2; //10 for testing, 2 normal
 
 let ball = {
-    x : boardWidth/2,
-    y : boardHeight/2,
-    width: ballWidth,
-    height: ballHeight,
-    velocityX : ballVelocityX,
-    velocityY : ballVelocityY
-}
+  x: boardWidth / 2,
+  y: boardHeight / 2,
+  width: ballWidth,
+  height: ballHeight,
+  velocityX: ballVelocityX,
+  velocityY: ballVelocityY,
+};
 
 //blocks
 let blockArray = [];
-let blockWidth = 50;
-let blockHeight = 10;
-let blockColumns = 8; 
+let blockWidth = 55;
+let blockHeight = 20;
+let blockColumns = 8;
 let blockRows = 3; //add more as game goes on
 let blockMaxRows = 10; //limit how many rows
 let blockCount = 0;
 
-//starting block corners top left 
+//starting block corners top left
 let blockX = 15;
 let blockY = 45;
 
 let score = 0;
 let gameOver = false;
 
-window.onload = function() {
-    board = document.getElementById("board");
-    board.height = boardHeight;
-    board.width = boardWidth;
-    context = board.getContext("2d"); //used for drawing on the board
+//save key press status
+let keys = {
+  ArrowLeft: false,
+  ArrowRight: false,
+};
 
-    //draw initial player
-    context.fillStyle="skyblue";
-    context.fillRect(player.x, player.y, player.width, player.height);
+window.onload = function () {
+  start = document.getElementById("game_start");
+  startMenu = document.getElementById("start_menu");
+  board = document.getElementById("board");
+  board.height = boardHeight;
+  board.width = boardWidth;
+  context = board.getContext("2d"); //used for drawing on the board
 
-    requestAnimationFrame(update);  // SetInterval과 비슷한 역할을 한다.
-    document.addEventListener("keydown", movePlayer);
+  start.onclick = function () {
+    startMenu.style.display = "none";
+    board.style.display = "block";
+  };
 
-    //create blocks
-    createBlocks(); 	// Block을 생성하는 역할
+  //draw initial player
+  context.fillStyle = "skyblue";
+  context.fillRect(player.x, player.y, player.width, player.height);
+
+  requestAnimationFrame(update); // SetInterval과 비슷한 역할을 한다.
+  document.addEventListener("keydown", (e) => {
+    if (e.code in keys) keys[e.code] = true;
+
+    if (gameOver && e.code === "Space") {
+      resetGame();
+    }
+  });
+
+  document.addEventListener("keyup", (e) => {
+    if (e.code in keys) keys[e.code] = false;
+  });
+
+  //create blocks
+  createBlocks(); // Block을 생성하는 역할
+};
+
+function movePlayer(e) {
+  if (gameOver) {
+    if (e.code == "Space") {
+      resetGame();
+      console.log("RESET");
+    }
+    return;
+  }
+  if (e.code == "ArrowLeft") {
+    // player.x -= player.velocityX;
+    let nextplayerX = player.x - player.velocityX;
+    if (!outOfBounds(nextplayerX)) {
+      player.x = nextplayerX;
+    }
+  } else if (e.code == "ArrowRight") {
+    let nextplayerX = player.x + player.velocityX;
+    if (!outOfBounds(nextplayerX)) {
+      player.x = nextplayerX;
+    }
+    // player.x += player.velocityX;
+  }
 }
 
 function update() {
-    requestAnimationFrame(update);
-    //stop drawing
-    if (gameOver) {
-        return;
-    }
-    context.clearRect(0, 0, board.width, board.height);
+  requestAnimationFrame(update);
+  //stop drawing
+  if (gameOver) {
+    return;
+  }
+  context.clearRect(0, 0, board.width, board.height);
 
-    // player
-    context.fillStyle = "lightgreen";
-    context.fillRect(player.x, player.y, player.width, player.height);
+  //player 움직임 처리리
+  if (keys.ArrowLeft) {
+    // player.x -= player.velocityX;
+    let nextplayerX = player.x - player.velocityX;
+    if (!outOfBounds(nextplayerX)) {
+      player.x = nextplayerX;
+    }
+  }
+  if (keys.ArrowRight) {
+    let nextplayerX = player.x + player.velocityX;
+    if (!outOfBounds(nextplayerX)) {
+      player.x = nextplayerX;
+    }
+    // player.x += player.velocityX;
+  }
 
-    // ball
-    context.fillStyle = "white";
-    ball.x += ball.velocityX;
-    ball.y += ball.velocityY;
-    context.fillRect(ball.x, ball.y, ball.width, ball.height);
+  // player
+  context.fillStyle = "lightgreen";
+  context.fillRect(player.x, player.y, player.width, player.height);
 
-    //bounce the ball off player paddle
-    if (topCollision(ball, player) || bottomCollision(ball, player)) {
-        ball.velocityY *= -1;   // flip y direction up or down
-    }
-    else if (leftCollision(ball, player) || rightCollision(ball, player)) {
-        ball.velocityX *= -1;   // flip x direction left or right
-    }
+  // ball
+  context.fillStyle = "white";
+  ball.x += ball.velocityX;
+  ball.y += ball.velocityY;
+  context.fillRect(ball.x, ball.y, ball.width, ball.height);
 
-    if (ball.y <= 0) { 
-        // if ball touches top of canvas
-        ball.velocityY *= -1; //reverse direction
-    }
-    else if (ball.x <= 0 || (ball.x + ball.width >= boardWidth)) {
-        // if ball touches left or right of canvas
-        ball.velocityX *= -1; //reverse direction
-    }
-    else if (ball.y + ball.height >= boardHeight) {
-        // if ball touches bottom of canvas
-        context.font = "20px sans-serif";
-        context.fillText("Game Over: Press 'Space' to Restart", 80, 400);
-        gameOver = true;
-    }
+  //bounce the ball off player paddle
+  if (topCollision(ball, player) || bottomCollision(ball, player)) {
+    ball.velocityY *= -1; // flip y direction up or down
+  } else if (leftCollision(ball, player) || rightCollision(ball, player)) {
+    ball.velocityX *= -1; // flip x direction left or right
+  }
 
-    //blocks
-    context.fillStyle = "skyblue";
-    for (let i = 0; i < blockArray.length; i++) {
-        let block = blockArray[i];
-        if (!block.break) {
-            if (topCollision(ball, block) || bottomCollision(ball, block)) {
-                block.break = true;     // block is broken
-                ball.velocityY *= -1;   // flip y direction up or down
-                score += 100;
-                blockCount -= 1;
-            }
-            else if (leftCollision(ball, block) || rightCollision(ball, block)) {
-                block.break = true;     // block is broken
-                ball.velocityX *= -1;   // flip x direction left or right
-                score += 100;
-                blockCount -= 1;
-            }
-            context.fillRect(block.x, block.y, block.width, block.height);
-        }
-    }
-
-    //next level
-    if (blockCount == 0) {
-        score += 100*blockRows*blockColumns; //bonus points :)
-        blockRows = Math.min(blockRows + 1, blockMaxRows);
-        createBlocks();
-    }
-
-    //score
+  if (ball.y <= 0) {
+    // if ball touches top of canvas
+    ball.velocityY *= -1; //reverse direction
+  } else if (ball.x <= 0 || ball.x + ball.width >= boardWidth) {
+    // if ball touches left or right of canvas
+    ball.velocityX *= -1; //reverse direction
+  } else if (ball.y + ball.height >= boardHeight) {
+    // if ball touches bottom of canvas
     context.font = "20px sans-serif";
-    context.fillText(score, 10, 25);
+    context.fillText("Game Over: Press 'Space' to Restart", 80, 400);
+    gameOver = true;
+  }
+
+  //blocks
+  context.fillStyle = "skyblue";
+  for (let i = 0; i < blockArray.length; i++) {
+    let block = blockArray[i];
+    if (!block.break) {
+      if (topCollision(ball, block) || bottomCollision(ball, block)) {
+        block.break = true; // block is broken
+        ball.velocityY *= -1; // flip y direction up or down
+        score += 100;
+        blockCount -= 1;
+      } else if (leftCollision(ball, block) || rightCollision(ball, block)) {
+        block.break = true; // block is broken
+        ball.velocityX *= -1; // flip x direction left or right
+        score += 100;
+        blockCount -= 1;
+      }
+      context.fillRect(block.x, block.y, block.width, block.height);
+    }
+  }
+
+  //next level
+  if (blockCount == 0) {
+    score += 100 * blockRows * blockColumns; //bonus points :)
+    blockRows = Math.min(blockRows + 1, blockMaxRows);
+    createBlocks();
+  }
+
+  //score
+  context.font = "20px sans-serif";
+  context.fillText(score, 10, 25);
 }
 
 function outOfBounds(xPosition) {
-    return (xPosition < 0 || xPosition + playerWidth > boardWidth);
-}
-
-function movePlayer(e) {
-    if (gameOver) {
-        if (e.code == "Space") {
-            resetGame();
-            console.log("RESET");
-        }
-        return;
-    }
-    if (e.code == "ArrowLeft") {
-        // player.x -= player.velocityX;
-        let nextplayerX = player.x - player.velocityX;
-        if (!outOfBounds(nextplayerX)) {
-            player.x = nextplayerX;
-        }
-    }
-    else if (e.code == "ArrowRight") {
-        let nextplayerX = player.x + player.velocityX;
-        if (!outOfBounds(nextplayerX)) {
-            player.x = nextplayerX;
-        }
-        // player.x += player.velocityX;    
-    }
+  return xPosition < 0 || xPosition + playerWidth > boardWidth;
 }
 
 function detectCollision(ball, block) {
-    return ball.x < block.x + block.width &&   //ball의 왼쪽 위 코너 corner doesn't reach b's top right corner
-           ball.x + ball.width > block.x &&   //a's top right corner passes b's top left corner
-           ball.y < block.y + block.height &&  //a's top left corner doesn't reach b's bottom left corner
-           ball.y + ball.height > block.y;    //a's bottom left corner passes b's top left corner
+  return (
+    ball.x < block.x + block.width && //ball의 왼쪽 위 코너 corner doesn't reach b's top right corner
+    ball.x + ball.width > block.x && //a's top right corner passes b's top left corner
+    ball.y < block.y + block.height && //a's top left corner doesn't reach b's bottom left corner
+    ball.y + ball.height > block.y
+  ); //a's bottom left corner passes b's top left corner
 }
 
-function topCollision(ball, block) { //a is above b (ball is above block)
-    return detectCollision(ball, block) && (ball.y + ball.height) >= block.y;
+function topCollision(ball, block) {
+  //a is above b (ball is above block)
+  return detectCollision(ball, block) && ball.y + ball.height >= block.y;
 }
 
-function bottomCollision(ball, block) { //a is above b (ball is below block)
-    return detectCollision(ball, block) && (block.y + block.height) >= ball.y;
+function bottomCollision(ball, block) {
+  //a is above b (ball is below block)
+  return detectCollision(ball, block) && block.y + block.height >= ball.y;
 }
 
-function leftCollision(ball, block) { //a is left of b (ball is left of block)
-    return detectCollision(ball, block) && (ball.x + ball.width) >= block.x;
+function leftCollision(ball, block) {
+  //a is left of b (ball is left of block)
+  return detectCollision(ball, block) && ball.x + ball.width >= block.x;
 }
 
-function rightCollision(ball, block) { //a is right of b (ball is right of block)
-    return detectCollision(ball, block) && (block.x + block.width) >= ball.x;
+function rightCollision(ball, block) {
+  //a is right of b (ball is right of block)
+  return detectCollision(ball, block) && block.x + block.width >= ball.x;
 }
 
-function createBlocks() { // 이 부분에서 난수로 0~10(?)개 정도 난수로 뽑아서 열로 내려오는 것을 구현하면 될 것 같습니다.
-    blockArray = []; //clear blockArray
-    for (let c = 0; c < blockColumns; c++) {
-        for (let r = 0; r < blockRows; r++) {
-            let block = {
-                x : blockX + c*blockWidth + c*10, //c*10 space 10 pixels apart columns
-                y : blockY + r*blockHeight + r*10, //r*10 space 10 pixels apart rows
-                width : blockWidth,
-                height : blockHeight,
-                break : false
-            }
-            blockArray.push(block);
-        }
+function createBlocks() {
+  // 이 부분에서 난수로 0~10(?)개 정도 난수로 뽑아서 열로 내려오는 것을 구현하면 될 것 같습니다.
+  blockArray = []; //clear blockArray
+  for (let c = 0; c < blockColumns; c++) {
+    for (let r = 0; r < blockRows; r++) {
+      let block = {
+        x: blockX + c * blockWidth + c * 2, //c*2 space 10 pixels apart columns
+        y: blockY + r * blockHeight + r * 2, //r*2 space 10 pixels apart rows
+        width: blockWidth,
+        height: blockHeight,
+        break: false,
+      };
+      blockArray.push(block);
     }
-    blockCount = blockArray.length;
+  }
+  blockCount = blockArray.length;
 }
 
-function resetGame() { // 게임 초기화
-    gameOver = false;
-    player = {
-        x : boardWidth/2 - playerWidth/2,
-        y : boardHeight - playerHeight - 5,
-        width: playerWidth,
-        height: playerHeight,
-        velocityX : playerVelocityX
-    }
-    ball = {
-        x : boardWidth/2,
-        y : boardHeight/2,
-        width: ballWidth,
-        height: ballHeight,
-        velocityX : ballVelocityX,
-        velocityY : ballVelocityY
-    }
-    blockArray = [];
-    blockRows = 3;
-    score = 0;
-    createBlocks();
+function resetGame() {
+  // 게임 초기화
+  gameOver = false;
+  player = {
+    x: boardWidth / 2 - playerWidth / 2,
+    y: boardHeight - playerHeight - 5,
+    width: playerWidth,
+    height: playerHeight,
+    velocityX: playerVelocityX,
+  };
+  ball = {
+    x: boardWidth / 2,
+    y: boardHeight / 2,
+    width: ballWidth,
+    height: ballHeight,
+    velocityX: ballVelocityX,
+    velocityY: ballVelocityY,
+  };
+  blockArray = [];
+  blockRows = 3;
+  score = 0;
+  createBlocks();
 }
