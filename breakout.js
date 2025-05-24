67734144e731d025e69eca9680418c07869042d2
정석현 <codjeongsh02@gmail.com>
2025-05-24 16:37:54 +0900

    벽돌 내려오는 것 구현 게임 canvas 좀 크게 바꿨습니다.

--- breakout.js
+++ breakout.js
@@ -1,15 +1,16 @@
-//board
+// 보듸의 크기
 let board;
-let boardWidth = 500;
-let boardHeight = 500;
+let boardWidth = 1000;
+let boardHeight = 650;
 let context; 
 
-//players
-let playerWidth = 80; //500 for testing, 80 normal
+// 플레이어의 크기 및 속력
+let playerWidth = 80;
 let playerHeight = 10;
-let playerVelocityX = 10; //move 10 pixels each time
+let playerVelocityX = 30; // 10pixel 정도로 움직임
 
-let player = {
+// 플레이어의 구조체
+let player = { // 
     x : boardWidth/2 - playerWidth/2,
     y : boardHeight - playerHeight - 5,
     width: playerWidth,
@@ -17,12 +18,13 @@ let player = {
     velocityX : playerVelocityX
 }
 
-//ball
+// 공의 크기 및 속도
 let ballWidth = 10;
 let ballHeight = 10;
 let ballVelocityX = 3; //15 for testing, 3 normal
 let ballVelocityY = 2; //10 for testing, 2 normal
 
+// ball의 구조체
 let ball = {
     x : boardWidth/2,
     y : boardHeight/2,
@@ -32,56 +34,77 @@ let ball = {
     velocityY : ballVelocityY
 }
 
-//blocks
-let blockArray = [];
-let blockWidth = 50;
-let blockHeight = 10;
-let blockColumns = 8; 
-let blockRows = 3; //add more as game goes on
-let blockMaxRows = 10; //limit how many rows
-let blockCount = 0;
+// enemy의 속성
+let enemyArray = [];
+let enemyWidth = 80;
+let enemyHeight = 80;
+let enemyColumns = 8; 
+let enemyRows = 3; //add more as game goes on
+let enemyMaxRows = 10; //limit how many rows
+let enemyCount = 0;
+let enemyVelocitX = 0;
+let enemyVelocitY = 0.1;
 
-//starting block corners top left 
-let blockX = 15;
-let blockY = 45;
+// enemy 정렬 시작 위치
+let enemyX = 15;
+let enemyY = 45;
 
+// 점수 계산
 let score = 0;
+
+// gameover 변수
 let gameOver = false;
 
-window.onload = function() {
+$(document).ready(function () {
     board = document.getElementById("board");
-    board.height = boardHeight;
     board.width = boardWidth;
-    context = board.getContext("2d"); //used for drawing on the board
+    board.height = boardHeight;
 
-    //draw initial player
-    context.fillStyle="skyblue";
+    context = board.getContext("2d"); // used for drawing on the board
+
+    // draw initial player
+    context.fillStyle = "skyblue";
     context.fillRect(player.x, player.y, player.width, player.height);
 
-    requestAnimationFrame(update);  // SetInterval과 비슷한 역할을 한다.
-    document.addEventListener("keydown", movePlayer);
+    $("#game_start").click(function() {
+      requestAnimationFrame(update);  // SetInterval과 비슷한 역할을 한다.
+      $(document).on("keydown", movePlayer);
 
-    //create blocks
-    createBlocks(); 	// Block을 생성하는 역할
-}
+      $("#game_ui").hide();
+      enemyAppear(); // enemy을 생성하는 역할
+    });
 
+
+});
+
+
+// Update 해줘야 할 것들 1. Enemy와 ball의 충돌 여부와 동시에 Enemy와 Player의 충돌 여부 2. Enemy의 내려옴 구현
 function update() {
     requestAnimationFrame(update);
     //stop drawing
     if (gameOver) {
         return;
     }
+    // 그린 것 초기화
     context.clearRect(0, 0, board.width, board.height);
 
-    // player
+    // player 그리기
     context.fillStyle = "lightgreen";
     context.fillRect(player.x, player.y, player.width, player.height);
 
-    // ball
+    // ball 그리기
     context.fillStyle = "white";
     ball.x += ball.velocityX;
     ball.y += ball.velocityY;
     context.fillRect(ball.x, ball.y, ball.width, ball.height);
+
+    // enemy 그리기
+    context.fillStyle = "red";
+    for (let i = 0; i < enemyArray.length; ++i) {
+      enemyArray[i].x += enemyVelocitX;
+      enemyArray[i].y += enemyVelocitY;
+      context.fillRect(enemyArray[i].x, enemyArray[i].y, enemyArray[i].width, enemyArray[i].height);
+    }
 
     //bounce the ball off player paddle
     if (topCollision(ball, player) || bottomCollision(ball, player)) {
@@ -106,32 +129,13 @@ function update() {
         gameOver = true;
     }
 
-    //blocks
-    context.fillStyle = "skyblue";
-    for (let i = 0; i < blockArray.length; i++) {
-        let block = blockArray[i];
-        if (!block.break) {
-            if (topCollision(ball, block) || bottomCollision(ball, block)) {
-                block.break = true;     // block is broken
-                ball.velocityY *= -1;   // flip y direction up or down
-                score += 100;
-                blockCount -= 1;
-            }
-            else if (leftCollision(ball, block) || rightCollision(ball, block)) {
-                block.break = true;     // block is broken
-                ball.velocityX *= -1;   // flip x direction left or right
-                score += 100;
-                blockCount -= 1;
-            }
-            context.fillRect(block.x, block.y, block.width, block.height);
-        }
-    }
+    enemyDraw();
 
     //next level
-    if (blockCount == 0) {
-        score += 100*blockRows*blockColumns; //bonus points :)
-        blockRows = Math.min(blockRows + 1, blockMaxRows);
-        createBlocks();
+    if (enemyCount == 0) {
+        score += 100*enemyRows*enemyColumns; //bonus points :)
+        enemyRows = Math.min(enemyRows + 1, enemyMaxRows);
+        createenemys();
     }
 
     //score
@@ -167,44 +171,76 @@ function movePlayer(e) {
     }
 }
 
-function detectCollision(ball, block) {
-    return ball.x < block.x + block.width &&   //ball의 왼쪽 위 코너 corner doesn't reach b's top right corner
-           ball.x + ball.width > block.x &&   //a's top right corner passes b's top left corner
-           ball.y < block.y + block.height &&  //a's top left corner doesn't reach b's bottom left corner
-           ball.y + ball.height > block.y;    //a's bottom left corner passes b's top left corner
+function detectCollision(ball, enemy) {
+    return ball.x < enemy.x + enemy.width &&   //ball의 왼쪽 위 코너 corner doesn't reach b's top right corner
+           ball.x + ball.width > enemy.x &&   //a's top right corner passes b's top left corner
+           ball.y < enemy.y + enemy.height &&  //a's top left corner doesn't reach b's bottom left corner
+           ball.y + ball.height > enemy.y;    //a's bottom left corner passes b's top left corner
 }
 
-function topCollision(ball, block) { //a is above b (ball is above block)
-    return detectCollision(ball, block) && (ball.y + ball.height) >= block.y;
+function topCollision(ball, enemy) { //a is above b (ball is above enemy)
+    return detectCollision(ball, enemy) && (ball.y + ball.height) >= enemy.y;
 }
 
-function bottomCollision(ball, block) { //a is above b (ball is below block)
-    return detectCollision(ball, block) && (block.y + block.height) >= ball.y;
+function bottomCollision(ball, enemy) { //a is above b (ball is below enemy)
+    return detectCollision(ball, enemy) && (enemy.y + enemy.height) >= ball.y;
 }
 
-function leftCollision(ball, block) { //a is left of b (ball is left of block)
-    return detectCollision(ball, block) && (ball.x + ball.width) >= block.x;
+function leftCollision(ball, enemy) { //a is left of b (ball is left of enemy)
+    return detectCollision(ball, enemy) && (ball.x + ball.width) >= enemy.x;
 }
 
-function rightCollision(ball, block) { //a is right of b (ball is right of block)
-    return detectCollision(ball, block) && (block.x + block.width) >= ball.x;
+function rightCollision(ball, enemy) { //a is right of b (ball is right of enemy)
+    return detectCollision(ball, enemy) && (enemy.x + enemy.width) >= ball.x;
 }
 
-function createBlocks() { // 이 부분에서 난수로 0~10(?)개 정도 난수로 뽑아서 열로 내려오는 것을 구현하면 될 것 같습니다.
-    blockArray = []; //clear blockArray
-    for (let c = 0; c < blockColumns; c++) {
-        for (let r = 0; r < blockRows; r++) {
-            let block = {
-                x : blockX + c*blockWidth + c*10, //c*10 space 10 pixels apart columns
-                y : blockY + r*blockHeight + r*10, //r*10 space 10 pixels apart rows
-                width : blockWidth,
-                height : blockHeight,
-                break : false
-            }
-            blockArray.push(block);
+
+function enemyAppear() {
+  createenemys();
+
+
+}
+function createenemys() { // 이 부분에서 난수로 0~10(?)개 정도 난수로 뽑아서 열로 내려오는 것을 구현하면 될 것 같습니다.
+    enemyArray = []; //clear enemyArray
+    for (let c = 0; c < enemyColumns; c++) {
+        for (let r = 0; r < enemyRows; r++) {
+            let x = enemyX + c * enemyWidth + c * 100; // c * 10 의 공간을 둔다. colum
+            let y = enemyY + r*enemyHeight + r*20 // r * 10 의 공간을 둔다. rows
+            let enemy = new Enemy(1, x, y, enemyVelocitX, enemyVelocitY);
+            // let enemy = {
+            //     x : enemyX + c*enemyWidth + c*10, //c*10 space 10 pixels apart columns
+            //     y : enemyY + r*enemyHeight + r*10, //r*10 space 10 pixels apart rows
+            //     width : enemyWidth,
+            //     height : enemyHeight,
+            //     break : false
+            // }
+            enemyArray.push(enemy);
         }
     }
-    blockCount = blockArray.length;
+    enemyCount = enemyArray.length;
+}
+
+function enemyDraw() {
+    //enemys
+    context.fillStyle = "skyblue";
+    for (let i = 0; i < enemyArray.length; i++) {
+        let enemy = enemyArray[i];
+        if (enemy.HP > 0) {
+            if (topCollision(ball, enemy) || bottomCollision(ball, enemy)) {
+                enemy.HP -= 1;     // enemy is broken
+                ball.velocityY *= -1;   // flip y direction up or down
+                score += 100;
+                enemyCount -= 1;
+            }
+            else if (leftCollision(ball, enemy) || rightCollision(ball, enemy)) {
+                enemy.HP -= 1;     // enemy is broken
+                ball.velocityX *= -1;   // flip x direction left or right
+                score += 100;
+                enemyCount -= 1;
+            }
+            context.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
+        }
+    }
 }
 
 function resetGame() { // 게임 초기화
@@ -224,8 +260,29 @@ function resetGame() { // 게임 초기화
         velocityX : ballVelocityX,
         velocityY : ballVelocityY
     }
-    blockArray = [];
-    blockRows = 3;
+    enemyArray = [];
+    enemyRows = 3;
     score = 0;
-    createBlocks();
+    createenemys();
 }
+
+class Enemy {
+  constructor(HP, x, y, velocityX, velocityY) {
+    this.HP = HP;
+    this.x = x; // 이름 수정
+    this.y = y; // 이름 수정
+    this.width = enemyWidth;
+    this.height = enemyHeight;
+    this.velocityX = velocityX;
+    this.velocityY = velocityY;
+  }
+
+  move_down() {
+    this.y += this.velocityY;
+  }
+
+  die() {
+    return this.HP <= 0;
+  }
+}
+
